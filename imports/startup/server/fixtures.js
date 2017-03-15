5231777b97c37d3af1f19b28601ae840e0ce66ec
sw-yx <shawnthe1@gmail.com>
2017-03-14 21:30:59 -0400

    trying to fix the accounts-password issues

--- imports/startup/server/fixtures.js
+++ imports/startup/server/fixtures.js
@@ -6,7 +6,7 @@ const users = [{
   email: 'admin@admin.com',
   password: 'password',
   profile: {
-    name: { first: 'Carl', last: 'Winslow' },
+    name: { first: 'Admin', last: 'Admin' },
   },
   roles: ['admin'],
 }];
